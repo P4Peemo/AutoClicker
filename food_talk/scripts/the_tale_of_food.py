99612184269565ca20898c42e2577f45ee6ee360
P4Peemo <eggtheclairvoyant@126.com>
2022-05-30 22:52:30 +0800

    Add app market path to .env

--- food_talk/scripts/the_tale_of_food.py
+++ food_talk/scripts/the_tale_of_food.py
@@ -1,22 +1,23 @@
 from schedule import run_pending, get_jobs
 from time import sleep
 from pyautogui import getWindowsWithTitle
+from dotenv import dotenv_values
 
 import food_talk.schedulers.cooking_scheduler
 import food_talk.schedulers.routine_scheduler
 from launch_game import launch_game, login
 
+config = dotenv_values()
 # TODO
 # formalize methods to be protected or public
 # add typing
 # logging
 # fail safe
 # routine work
-target_location = 'D:/Program Files/TxGameAssistant/AppMarket/AppMarket.exe'
 
 if __name__ == '__main__':
     if not getWindowsWithTitle('Gameloop'):
-        launch_game(target_location)
+        launch_game(config['APP_MARKET_PATH'])
         login()
     all_jobs = get_jobs()
     print(f'{len(all_jobs)} tasks scheduled.')
